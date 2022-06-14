e35c5937aa786a66656e7c7f90fa08c8af6a7e4b
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-06-14 15:03:10 +0545

    Creating new auth flow

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const userRouter = require("./routes/userRoutes");
-const globalErrorHandler = require('./controller/errorController');
-const AppError = require('./utils/appError');
-const moment = require('moment');
+const cookiePraser = require("cookie-parser");
+const globalErrorHandler = require("./controller/errorController");
+const AppError = require("./utils/appError");
+const moment = require("moment");
 
-dotenv.config({path: "./config.env"});
+dotenv.config({ path: "./config.env" });
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookiePraser());
 
 connectDB();
 
 app.use("/api/v1/user", userRouter);
 
 app.use("*", (req, res, next) => {
-    next(new AppError(`Cannot find ${req.originalUrl} in this server`, 404));
+  next(new AppError(`Cannot find ${req.originalUrl} in this server`, 404));
 });
 
 app.use(globalErrorHandler);
@@ -25,5 +28,5 @@ app.use(globalErrorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
