dcc6da475be18df2a8c93621065865f33b8e7ce5
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-06-26 18:04:27 +0545

    added logout route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/createUser", authController.createUser);
 router.post("/signin", authController.signIn);
 router.post("/googleLogin", authController.googleLogin);
+router.post("/logout", authMiddleware.protect, authController.logout);
 router.get("/getAllUsers", authMiddleware.protect, userController.getAllUsers);
 
 module.exports = router;
