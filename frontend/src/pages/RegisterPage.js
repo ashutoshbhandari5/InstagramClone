491630870e3d342684adb4e069e6a0876341a896
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-06-28 18:00:59 +0545

    created register components

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <section className="register-page">
+      <main className="register">Regiser Page signUp</main>
+    </section>
+  );
+};
+
+export default RegisterPage;
